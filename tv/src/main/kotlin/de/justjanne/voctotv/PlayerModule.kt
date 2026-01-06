88fff0fcebb6ef38f8f9f421425b1dd4bfd8c3f8
Janne Mareike Koschinski <mail@justjanne.de>
2026-01-06 10:08:08 +0100

    fix lint issues

--- tv/src/main/kotlin/de/justjanne/voctotv/PlayerModule.kt
+++ tv/src/main/kotlin/de/justjanne/voctotv/PlayerModule.kt
@@ -1,7 +1,9 @@
 package de.justjanne.voctotv
 
 import android.content.Context
+import androidx.annotation.OptIn
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.ScrubbingModeParameters
 import androidx.media3.session.MediaSession
@@ -14,6 +16,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 @Module
 @InstallIn(ViewModelComponent::class)
 internal object PlayerModule {
+    @OptIn(UnstableApi::class)
     @Provides
     fun providePlayer(
         @ApplicationContext context: Context
